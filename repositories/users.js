ef77ea26eaad579f0a00815ddb7987e58ea63016
Matthew Henderson <info@matthewahenderson.com>
2022-06-25 10:01:15 -0400

    Added getAll method to retrieve users from file; added create method to add records

--- repositories/users.js
+++ repositories/users.js
@@ -12,7 +12,33 @@ class UsersRepository {
         } catch (err) {
             fs.writeFileSync(this.filename, '[]');
         }
-    };
+    }
+
+    async getAll() {
+        // Open the file specified, parse the data, and return it
+        return JSON.parse(
+            await fs.promises.readFile(this.filename, {
+                encoding: 'utf8'
+        }));
+    }
+
+    async create(attrs) {
+        const records = await this.getAll();
+        records.push(attrs);
+
+        // Write back to the JSON file
+        await fs.promises.writeFile(this.filename, JSON.stringify(records));
+    }
 }
 
-new UsersRepository();
+const test = async () => {
+    const repo = new UsersRepository('users.json');
+
+    await repo.create({ email: 'test@test.com', password: 'password' });
+
+    const users = await repo.getAll();
+
+    console.log(users);
+};
+
+test();
