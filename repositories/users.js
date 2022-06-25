2987ea80d72efc083a50cc88ab316477e2c31c37
Matthew Henderson <info@matthewahenderson.com>
2022-06-25 12:13:06 -0400

    Added update, delete, and getOneBy methods; exported class for use in other files; added signup validation logic

--- repositories/users.js
+++ repositories/users.js
@@ -48,14 +48,42 @@ class UsersRepository {
         const records = await this.getAll();
         return records.find(record => record.id === id);
     }
+
+    async delete(id) {
+        const records = await this.getAll();
+        const filteredRecords = records.filter(record => record.id !== id);
+        await this.writeAll(filteredRecords);
+    }
+
+    async update(id, attrs) {
+        const records = await this.getAll();
+        const record = records.find(record => record.id === id);
+
+        if (!record) {
+            throw new Error(`Record with id ${id} not found`);
+        }
+
+        Object.assign(record, attrs);
+        await this.writeAll(records);
+    }
+
+    async getOneBy(filters) {
+        const records = await this.getAll();
+
+        for (let record of records) {
+            let found = true;
+
+            for (let key in filters) {
+                if (record[key] !== filters[key]) {
+                    found = false;
+                }
+            }
+
+            if (found) {
+                return record;
+            }
+        }
+    }
 }
 
-const test = async () => {
-    const repo = new UsersRepository('users.json');
-
-    const user = await repo.getOne('719f3db6');
-
-    console.log(user);
-};
-
-test();
+module.exports = new UsersRepository('users.json ')
