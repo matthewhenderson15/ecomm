c841a8447193d6bf8a68826dfe6ac15beac4fc97
Matthew Henderson <info@matthewahenderson.com>
2022-06-26 20:20:26 -0400

    Added comparePasswords method; added hashing + salt to passwords

--- repositories/users.js
+++ repositories/users.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 const crypto = require('crypto');
+const util = require('util');
+
+const scrypt = util.promisify(crypto.scrypt);
 
 class UsersRepository {
     constructor(filename) {
@@ -25,14 +28,30 @@ class UsersRepository {
 
     async create(attrs) {
         attrs.id = this.randomId();
+
+        const salt = crypto.randomBytes(8).toString('hex');
+        const buf = await scrypt(attrs.password, salt, 64);
         
         const records = await this.getAll();
-        records.push(attrs);
+        const record = {
+            ...attrs,
+            password: `${buf.toString('hex')}.${salt}`
+        };
+        records.push(record);
 
         // Write back to the JSON file
         await this.writeAll(records);
 
-        return attrs;
+        return record;
+    }
+
+    async comparePasswords(saved, supplied) {
+        // Saved is password in db -> 'hashed.salt'
+        // Supplied is password given by user trying to sign in 
+        const [hashed, salt] = saved.split('.');
+        const hashSuppliedBuf = await scrypt(supplied, salt, 64);
+
+        return hashed === hashSuppliedBuf.toString('hex');
     }
 
     async writeAll(records) {
